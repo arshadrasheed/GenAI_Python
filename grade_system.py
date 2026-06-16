4b9efb4977fd470780ba6e8be05512e6f2504da3
Mohamed Arshad <70330961+arshadrasheed@users.noreply.github.com>
2026-06-17 00:26:14 +0530

    - Minor Changes added

--- grade_system.py
+++ grade_system.py
@@ -12,29 +12,42 @@ def GetInput():
     number= input()
     return number
 
+#Grade Constants
+grade=("A","B","C","D","E")
+
 #Function Valdate the Mark
 def ValidateMark(mark):
     if mark >=90 and mark<=100:
-        return "A"
+        PrintResult(mark,grade[0])
     elif mark >=80 and mark<90:
-        return "B"
+        PrintResult(mark,grade[1])
     elif mark >=70 and mark<80:
-        return "C"
+        PrintResult(mark,grade[2])
     elif mark >=60 and mark<70:
-        return "D"
+        PrintResult(mark,grade[3])
     elif mark>=0 and mark<60:
-        return "E"
+        PrintResult(mark,grade[4])
     else:
         return "Enter Mark within [0-100]"
     
+def PrintResult(mark, grade):
+     print("--------------------")
+     print("Your Mark is ",mark)
+     print("Your Grade is ",grade)
+     print("--------------------")
+
 #Grade Calcualtion
 def GradeSystem():
+
+    #Get Input from User
     mark= GetInput()
 
+    #Validate the input is number or not
     isDigit= IsNumber(mark)
 
+    #Validate the Mark
     if isDigit:
-      print(ValidateMark(int(mark)))  
+      ValidateMark(int(mark))
     else:
         print("Enter Valid Digit")
 
